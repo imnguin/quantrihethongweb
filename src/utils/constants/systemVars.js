367d492dbb544c35c989a7c4916dc65af7a78fd2
Lâm Xuân Nguyên <100450698+imnguin@users.noreply.github.com>
2025-12-26 15:07:23 +0700

    Update systemVars.js

--- src/utils/constants/systemVars.js
+++ src/utils/constants/systemVars.js
@@ -2,7 +2,8 @@ export const HOSTNAME = 'NODESERVER'
 export const HOST_LIST = {
     [HOSTNAME]: {
         hostName: [HOSTNAME],
-        hostBaseURL : 'http://localhost:8080/'
+        // hostBaseURL : 'http://localhost:8080/'
+        hostBaseURL: 'https://quantrihethongserver.onrender.com/'
     }
 }
 export const GROUPDISPLAY = [2844, 2848, 2855, 3020, 4259, 4260, 3053, 3934, 3931]
